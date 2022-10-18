f998919028f9cd7adf4f2a3b1b033976c62b4a2a
Tebbot <85313940+Tebbot@users.noreply.github.com>
2022-10-18 19:20:24 +0100

    Update start.js

--- start.js
+++ start.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 
 mongoose.connect(process.env.CONNECTIONSTRING).then(() => {
     console.log("Connected to db! Starting backend.")
-    childProcess.fork(`${__dirname}/backend/index`)
+    console.log(__dirname)
 })
 
 
